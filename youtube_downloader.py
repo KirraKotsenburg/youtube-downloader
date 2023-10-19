0a616e307789d698f9a1d3adc4a40c0ea8756be7
KirraKotsenburg <kirrakotz@hotmail.com>
2023-10-19 00:42:40 -0600

    working-downloader

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,3 +1,5 @@
+# To download a YouTube video type in the commandline: python youtube_downloader.py <"YouTube link">
+#
 from pytube import YouTube
 from sys import argv
 
@@ -5,8 +7,11 @@ link = argv[1] # First command line argument that we give
 
 yt = YouTube(link) # YouTube object with our argument (link)
 
+# For printing the video's title and author
 print("Title: ", yt.title)
 
-print("Views: ", yt.views)
+print("Author: ", yt.author)
 
+yd = yt.streams.get_highest_resolution() # downloads highest resolation of video
 
+yd.download('/Users/kirra/OneDrive/YouTube_Downloads') # The folder you want the downloads to go to
